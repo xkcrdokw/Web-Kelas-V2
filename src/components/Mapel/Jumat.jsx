3f095f65e69bd2d70cd6216cf6ce617f3daaffee
xkcrdokw <nashrul2969@gmail.com>
2025-02-27 15:51:06 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -5,36 +5,36 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TJKT Server-Cisco</div>
-				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
+				<div className="w-[50%] text-base  font-medium">MO</div>
+				<div className="flex justify-center items-center text-sm">08.30-09.50</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
 				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">09.40-10.00</div>
+				<div className="flex justify-center items-center text-sm">09.50-10.05</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">Mikrotik</div>
-				<div className="flex justify-center items-center text-sm">10.00-11.20</div>
+				<div className="w-[50%] text-base  font-medium">MO</div>
+				<div className="flex justify-center items-center text-sm">10.05.11.25</div>
 			</div>
 			<div
 				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
 				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">11.20-13.00</div>
+				<div className="flex justify-center items-center text-sm">11.25-12.40</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TJKT Jaringan Lanjutan</div>
-				<div className="flex justify-center items-center text-sm">13.00-14.20</div>
+				<div className="w-[50%] text-base  font-medium">MO</div>
+				<div className="flex justify-center items-center text-sm">12.40-14.00</div>
 			</div>
 		</>
 	)

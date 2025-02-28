13bd99be0ccc76392e7737a9dfb65c24700cd434
xkcrdokw <nashrul2969@gmail.com>
2025-02-28 13:57:41 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore"
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
-  authDomain: "project-web-kelas.firebaseapp.com",
-  projectId: "project-web-kelas",
-  storageBucket: "project-web-kelas.appspot.com",
-  messagingSenderId: "202691312352", 
-  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
+  apiKey: "AIzaSyAtJ8ZY0j2xKI0HV_X_CIx8bIAW9kTiRSI",
+  authDomain: "web-kelas-tes-d9d0a.firebaseapp.com",
+  projectId: "web-kelas-tes-d9d0a",
+  storageBucket: "web-kelas-tes-d9d0a.firebasestorage.app",
+  messagingSenderId: "128993362509",
+  appId: "1:128993362509:web:d53d13328e2cee491b5204",
+  measurementId: "G-6NZMY5Y8PL"
 
 
 };

15268627b3aa98d7e19fe214e42484847070c92f
xkcrdokw <nashrul2969@gmail.com>
2025-02-28 17:21:44 +0700

    Update AnggotaKelas.jsx

--- src/components/AnggotaKelas.jsx
+++ src/components/AnggotaKelas.jsx
@@ -1,49 +1,11 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
+import Fade from "@mui/material/Fade";
 import CloseIcon from "@mui/icons-material/Close";
 import Button from "@mui/material/Button";
-
-
-const Fade = React.forwardRef(function Fade(props, ref) {
-	const { children, in: open, onClick, onEnter, onExited, ownerState, ...other } = props
-	const style = useSpring({
-		from: { opacity: 0 },
-		to: { opacity: open ? 1 : 0 },
-		config: {
-		  duration: open ? 200 : 50, // Mengatur durasi berdasarkan kondisi open
-		},
-		onStart: () => {
-		  if (open && onEnter) {
-			onEnter(null, true);
-		  }
-		},
-		onRest: () => {
-		  if (!open && onExited) {
-			onExited(null, true);
-		  }
-		},
-	  });
-	  
-	  
-	return (
-		<animated.div ref={ref} style={style} {...other}>
-			{React.cloneElement(children, { onClick })}
-		</animated.div>
-	)
-})
-
-Fade.propTypes = {
-	children: PropTypes.element.isRequired,
-	in: PropTypes.bool,
-	onClick: PropTypes.any,
-	onEnter: PropTypes.func,
-	onExited: PropTypes.func,
-	ownerState: PropTypes.any,
-}
-
 
 export default function AnggotaKelas() {
   const [open, setOpen] = useState(false);
@@ -93,17 +55,14 @@ export default function AnggotaKelas() {
         </div>
       </Button>
       <Modal
-				aria-labelledby="spring-modal-title"
-				aria-describedby="spring-modal-description"
-				open={open}
-				onClose={handleClose}
-				closeAfterTransition
-				slots={{ backdrop: Backdrop }}
-				slotProps={{
-					backdrop: {
-						TransitionComponent: Fade,
-					},
-				}}>
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{ timeout: 500 }}
+      >
         <Fade in={open}>
           <Box className="modal-container">
             <Button onClick={handleClose} style={{ position: "absolute", top: "0", right: "0" }}>
@@ -128,4 +87,4 @@ export default function AnggotaKelas() {
       </Modal>
     </div>
   );
-      }
+		    }

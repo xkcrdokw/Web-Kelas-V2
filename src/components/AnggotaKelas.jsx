483ac3afce17b232eaa855d153716777745d64bc
xkcrdokw <nashrul2969@gmail.com>
2025-02-28 17:42:54 +0700

    Update AnggotaKelas.jsx

--- src/components/AnggotaKelas.jsx
+++ src/components/AnggotaKelas.jsx
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
-import Fade from "@mui/material/Fade";
-import CloseIcon from "@mui/icons-material/Close";
-import Button from "@mui/material/Button";
+import * as React from "react"
+import PropTypes from "prop-types"
+import Backdrop from "@mui/material/Backdrop"
+import Box from "@mui/material/Box"
+import Modal from "@mui/material/Modal"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { useSpring, animated } from "@react-spring/web"
+import CloseIcon from "@mui/icons-material/Close"
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { children, in: open, onClick, onEnter, onExited, ownerState, ...other } = props
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    config: {
+      duration: open ? 200 : 50,
+    },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter(null, true)
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited(null, true)
+      }
+    },
+  })
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {React.cloneElement(children, { onClick })}
+    </animated.div>
+  )
+})
+
+Fade.propTypes = {
+  children: PropTypes.element.isRequired,
+  in: PropTypes.bool,
+  onClick: PropTypes.any,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+  ownerState: PropTypes.any,
+}
 
 export default function AnggotaKelas() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
   const anggotaKelas = [
     "Afdal",
     "Andre",
@@ -43,14 +80,13 @@ export default function AnggotaKelas() {
     "Xena",
     "Yoga",
     "Zara",
-  ];
+  ]
 
   return (
     <div>
       <Button onClick={handleOpen}>
         <div className="flex items-center space-x-2 text-white px-6 py-4">
-          <span>AnggotaKelas</span>
-          <img src="/avatar.png" alt="icon" className="w-6 h-6" />
+          <span>Anggota Kelas</span>
         </div>
       </Button>
       <Modal
@@ -60,11 +96,7 @@ export default function AnggotaKelas() {
         onClose={handleClose}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
+        slotProps={{ backdrop: { TransitionComponent: Fade } }}
       >
         <Fade in={open}>
           <Box className="modal-container">
@@ -89,5 +121,5 @@ export default function AnggotaKelas() {
         </Fade>
       </Modal>
     </div>
-  );
-	  }
+  )
+}

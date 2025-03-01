22fae06ff7b998a1157ee4b4c8eaed3632d2a3fd
xkcrdokw <nashrul2969@gmail.com>
2025-03-01 11:18:32 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
     <div className="text-white px-[10%]" id="Home">
       <Navbar />
       <div className="lg:hidden">
-        <div className="font-bold text-5xl text-center py-20 ">
+        <div className="font-bold text-5xl text-center py-15 ">
           XI TKR 4
         </div>
-        <div className="font-bold text-sm text-center pb-8">2023 - 2026</div>
+        <div className="font-bold text-sm text-center pb-5">2023 - 2026</div>
         <div className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative" id="TotalSiswa">
           <div id="AngkaGradientBlue" className="flex items-center justify-center text-6xl">
             35
